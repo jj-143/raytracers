2ea337c21ac28d7aad2a3820a57afe9a54c0fa74
JJ Kim <jj.dev.001@gmail.com>
2023-12-29 14:11:29 +0900

    S2: draw sphere (mine)

--- main.cpp
+++ main.cpp
@@ -6,15 +6,33 @@
 
 #include "geometry.h"
 
+struct Sphere {
+  Vec3f pos;
+  int radius;
+};
+
 void render() {
   const int width = 1024;
   const int height = 768;
   std::vector<Vec3f> framebuffer(width * height);
 
+  const Sphere sphere = {Vec3f(100, 200, 100), 50};
+
   for (size_t j = 0; j < height; j++) {
     for (size_t i = 0; i < width; i++) {
       framebuffer[i + j * width] =
           Vec3f(j / float(height), i / float(width), 0);
+    }
+  }
+
+  for (size_t j = 0; j < height; j++) {
+    for (size_t i = 0; i < width; i++) {
+      const int ds = (j - sphere.pos[0]) * (j - sphere.pos[0]) +
+                     (i - sphere.pos[1]) * (i - sphere.pos[1]);
+
+      if (ds <= sphere.radius * sphere.radius) {
+        framebuffer[i + j * width] = Vec3f(0.5f, 0.5f, 0.5f);
+      }
     }
   }
 
