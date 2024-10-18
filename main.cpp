532b548cc45026592a2d61b585372aee3c753f18
JJ Kim <jj.dev.001@gmail.com>
2024-10-18 18:13:58 +0900

    setup: Reset the Scene as v2 - Cornell Box

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ inline float randf() {
   return static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
 }
 
-const Vec3f BACKGROUND_COLOR = Vec3f(0.2, 0.7, 0.8);
+const Vec3f BACKGROUND_COLOR = Vec3f(0, 0, 0);
 const int MAX_REFLECTION_DEPTH = 4;
 const int MAX_REFRACTION_DEPTH = 12;
 const int N_SAMPLES = 4;
@@ -148,40 +148,51 @@ bool cast_ray(const Vec3f &orig, const Vec3f dir, Vec3f &color,
 }
 
 void render(const int &width, const int &height) {
-  // cam
-  float fov = 50 * M_PI / 180;
+  // Camera
+  Vec3f cam_orig = {.275, .275, .8};
+
+  // 35mm lens with 25mm sensor
+  float fov = 39.3076 * M_PI / 180;
   float pixel_size = tan(fov / 2) / width;
 
   std::vector<Vec3f> framebuffer(width * height);
   std::vector<float> depthbuffer(width * height);
 
   std::vector<PointLight> lights;
-  lights.push_back({Vec3f(0, 1.2, -.5), 0.8});
-  lights.push_back({Vec3f(2.8, 1.4, -.4), 0.6});
+  lights.push_back({Vec3f(.275, .548, -.275), 1.5});
 
   std::vector<Object> objects;
 
   // clang-format off
-  Sphere s1       = {Vec3f(  .2,  -.2, -2.3),   0.2}; // red
-  Sphere s2       = {Vec3f( -.2,   .1,   -2),  0.25}; // green
-  Sphere s3       = {Vec3f(  .4,   .3, -2.3),   0.3}; // mirror
-  Sphere s4       = {Vec3f( -.1,  -.2, -1.4),   0.2}; // glass
-  Sphere s5       = {Vec3f(  .1,  -.2, -1.8),   0.1}; // yellow
-  Plane plane     = {Vec3f(   0,  -.3,   -3),   .8,   .8, Vec3f(M_PI / 6, 0, 0)};
+  Sphere s1        = {Vec3f(.425, .080, -.344), .08}; // red
+  Sphere s2        = {Vec3f(.125, .080, -.344), .08}; // green
+  Sphere s3        = {Vec3f(.195, .275, -.470), .08}; // yellow
+  Sphere s4        = {Vec3f(.355, .275, -.470), .08}; // mirror
+  Sphere s5        = {Vec3f(.275, .080, -.140), .08}; // glass
+  Plane floor      = {Vec3f(.275,    0, -.275), .275, .275, Vec3f(0, 1, 0)};
+  Plane ceiling    = {Vec3f(.275,  .55, -.275), .275, .275, Vec3f(0, -1, 0)};
+  Plane left_wall  = {Vec3f(   0, .275, -.275), .275, .275, Vec3f(1, 0, 0)};
+  Plane right_wall = {Vec3f( .55, .275, -.275), .275, .275, Vec3f(-1, 0, 0)};
+  Plane back_wall  = {Vec3f(.275, .275,  -.55), .275, .275, Vec3f(0, 0, 1)};
 
-  Material red        = Material{Vec3f(  1,  0,  0), {  .6,  .3,  .1,   0},  100,    1};
-  Material green      = Material{Vec3f(  0,  1,  0), {  .6,  .3,  .1,   0},   20,    1};
-  Material yellow     = Material{Vec3f(  1,  1,  0), {  .6,  .3,  .1,   0},  100,    1};
+  Material red        = Material{Vec3f(  1,  0,  0), {  .6,  .3,  .01,   0},  100,    1};
+  Material green      = Material{Vec3f(  0,  1,  0), {  .6,  .3,  .01,   0},   20,    1};
+  Material yellow     = Material{Vec3f(  1,  1,  0), {  .6,  .3,  .01,   0},  100,    1};
+  Material white      = Material{Vec3f(  1,  1,  1), {  .6,  .1,  .01,   0},   10,    1};
   Material mirror     = Material{Vec3f(  1,  1,  1), {   0,  10,  .8,   0}, 1500,    1};
-  Material glass      = Material{Vec3f( .6, .7, .8), {  .1,  10,  .1,  .8}, 1500,  1.5};
-  Material mat_plane  = Material{Vec3f(  1,  1,  1), {  .6,  .3,  .1,   0},   10,    1};
+  Material glass      = Material{Vec3f( .6, .7, .8), {  .1,  10,  .01,  .8}, 1500,  1.5};
   // clang-format on
 
   objects.push_back({s1, red});
   objects.push_back({s2, green});
-  objects.push_back({s3, mirror});
-  objects.push_back({s4, glass});
-  objects.push_back({s5, yellow});
+  objects.push_back({s3, yellow});
+  objects.push_back({s4, mirror});
+  objects.push_back({s5, glass});
+  objects.push_back({floor, white});
+  objects.push_back({ceiling, white});
+  objects.push_back({left_wall, red});
+  objects.push_back({right_wall, green});
+  objects.push_back({back_wall, white});
 
   int64_t now = nowMillis();
 
@@ -203,7 +214,7 @@ void render(const int &width, const int &height) {
 
         Vec3f dir = Vec3f(x, y, -1).normalize();
 
-        if (!cast_ray(Vec3f(0, 0, 0), dir, sample_color, objects, lights)) {
+        if (!cast_ray(cam_orig, dir, sample_color, objects, lights)) {
           sample_color = BACKGROUND_COLOR;
         }
         color = color + sample_color;
@@ -219,8 +230,8 @@ void render(const int &width, const int &height) {
 }
 
 int main(int argc, char *argv[]) {
-  const int width = argc > 1 ? atoi(argv[1]) : 1024;
-  const int height = argc > 2 ? atoi(argv[2]) : 768;
+  const int width = argc > 1 ? atoi(argv[1]) : 512;
+  const int height = argc > 2 ? atoi(argv[2]) : 512;
 
   printf("Frame size: (%d, %d)\n", width, height);
 
