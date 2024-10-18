af7218d3bdb2a727eaf785278358f9720a9ff698
JJ Kim <jj.dev.001@gmail.com>
2024-10-18 17:36:11 +0900

    Support setting face normal to Plane

--- common.h
+++ common.h
@@ -41,19 +41,15 @@ class Plane : public Mesh {
  public:
   float half_width;
   float half_height;
-  Vec3f rotation;  // currently, only X rot is allowed.
-  Vec3f face_normal = Vec3f(0, 1, 0);
+  Vec3f normal = Vec3f(0, 1, 0);
 
   bool ray_intersect(const Vec3f &orig, const Vec3f &dir, float &t0,
                      Vec3f &N) const override {
-    Vec3f normal = Vec3f(0, cos(rotation.x), sin(rotation.x));
     Vec3f L = (pos - orig);
     float d = L * normal * (1 / (dir * normal));
     Vec3f r = dir * d - L;
 
-    // split component
-    Vec3f dir_w = Vec3f(1, 0, 0);
-    Vec3f dir_h = Vec3f(0, -sin(rotation.x), cos(rotation.x));
+    // split components
     float w = r * dir_w;
     float h = r * dir_h;
 
@@ -68,8 +64,23 @@ class Plane : public Mesh {
     }
   }
 
-  Plane(Vec3f p, float w, float h, Vec3f rot)
-      : Mesh(p), half_width(w), half_height(h), rotation(rot) {}
+  Plane(Vec3f p, float w, float h, Vec3f normal)
+      : Mesh(p), half_width(w), half_height(h), normal(normal) {
+    this->normal = normal.normalize();
+    Vec3f up = {0, 1, 0};
+    Vec3f right = cross(up, this->normal);
+
+    // if normal is almost +Y/-Y: use {1, 0, 0} as dir_w
+    // else: use right as dir_w
+    if (right.norm() != 0) {
+      dir_w = right.normalize();
+    }
+    dir_h = cross(this->normal, dir_w);
+  }
+
+ private:
+  Vec3f dir_w = Vec3f(1, 0, 0);
+  Vec3f dir_h = Vec3f(0, 0, -1);
 };
 
 struct Material {
