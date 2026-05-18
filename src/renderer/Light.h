730cfd81f7b6aea040eff51c49d38d4c7472311b
JJ Kim <jj.dev.001@gmail.com>
2026-05-18 10:42:26 +0900

    Add Light interface for Emitter type SceneObject

---
+++ src/renderer/Light.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <concepts>
+
+#include "SceneObject.h"
+#include "helpers.h"
+
+template <typename T>
+concept Emitter = requires(T t) {
+  { t.L() } -> std::same_as<Vec3f>;
+};
+
+bool constexpr IsLight(SceneObject* obj) {
+  if (!obj) return false;
+  return obj->visit(overloaded{
+      [](Emitter auto&&) { return true; },
+      [](auto&&) { return false; },
+  });
+}
+
+// Light interface to use with SceneObject
+class Light {
+ public:
+  Light(SceneObject* obj) : obj(obj) {}
+
+  Vec3f L() {
+    return obj->visit(overloaded{
+        [](Emitter auto&& l) { return l.L(); },
+        [](auto&&) { return Vec3f{0}; },
+    });
+  }
+
+ private:
+  SceneObject* obj;
+};
